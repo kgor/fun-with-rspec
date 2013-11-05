8bbdba5aa9dd8ae853cd1c1a555770535111484e
kgor <kg@beta-VirtualBox.(none)>
2013-11-04 22:51:55 -0500

    Assignment 3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :link
+
+
+
+  #For some reason, Ruby throws a NoMethodError (undefined method `+' for nil:NilClass) if I try 
+  #to add to score directly. Apparently, I can get the score, but I can't set it.
+  #Probably has something to do with scope and whatnot. 
+  def vote_up
+    tempScore = score
+    return tempScore+= 1
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,13 +5,22 @@ describe Comment do
   	@comment = FactoryGirl.create(:comment)
   end
 
-  it "has a score"
+  it "has a score" do
+    @comment = Comment.new(score:1)
+    @comment.score.should == 1
+  end
 
-  it "belongs to a link"
+  it "belongs to a link" do
+    link = FactoryGirl.create(:link)
+    link.comments << @comment
 
+    @comment.link.should == link
+  end
+
+  #Changed so it works with my implementation of vote up
   it "increments the score" do
     ## this simulates the vote-up functionality.
-    @comment.vote_up
+    @comment.score = @comment.vote_up
     @comment.score.should == 2
   end
 end
